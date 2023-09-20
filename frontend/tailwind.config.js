1e7c655fe3bde0050605c9d5f5eb76e23110338a
Sergionx <sergio180203@gmail.com>
2023-09-19 23:43:51 -0400

    added palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f5f2ff",
+          100: "#eee7ff",
+          200: "#dfd2ff",
+          300: "#c8afff",
+          400: "#af81ff",
+          500: "#984eff",
+          600: "#8d2afd",
+          700: "#7f18e9",
+          800: "#6914c3",
+          900: "#5812a0",
+          950: "#330867",
+        },
+
+        secondary: {
+          50: "#f0fdfc",
+          100: "#cdfaf6",
+          200: "#9bf4ef",
+          300: "#61e7e4",
+          400: "#30cfd0",
+          500: "#18b0b4",
+          600: "#108c91",
+          700: "#116f74",
+          800: "#13575c",
+          900: "#14494d",
+          950: "#052a2e",
+        },
+      },
+    },
   },
   plugins: [],
 };
+
+// Palette https://uicolors.app/edit?sv1=windsor:50-f5f2ff/100-eee7ff/200-dfd2ff/300-c8afff/400-af81ff/500-984eff/600-8d2afd/700-7f18e9/800-6914c3/900-5812a0/950-330867;turquoise:50-f0fdfc/100-cdfaf6/200-9bf4ef/300-61e7e4/400-30cfd0/500-18b0b4/600-108c91/700-116f74/800-13575c/900-14494d/950-052a2e
