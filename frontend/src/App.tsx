6c00852fe27dda2e7d1c2187e6bc013932fc148d
Sergionx <sergio180203@gmail.com>
2023-09-20 10:31:35 -0400

    better layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -63,7 +63,11 @@ export default function App() {
           grid place-items-center"
       >
         <Blobs />
-        <div className="backdrop-blur-md bg-secondary-200/50 p-8 rounded-md">
+        <div
+          className="flex flex-col backdrop-blur-md bg-secondary-200/50 p-8 rounded-md
+          h-[80vh] w-[80vw] max-w-[600px] max-h-[600px]
+        "
+        >
           <h1 className="text-4xl font-bold mb-6">Private Chat</h1>
 
           <ul className="flex flex-col gap-2 mb-6">
@@ -73,41 +77,38 @@ export default function App() {
                 className={`p-2 rounded-lg
                 ${
                   message.id === "Me"
-                    ? "bg-primary-500/90 rounded-tr-none"
+                    ? "bg-primary-400/90 rounded-tr-none"
                     : "bg-primary-300/90 rounded-tl-none"
                 }`}
               >
-                {message.id}: {message.body}
+                {message.body}
               </li>
             ))}
           </ul>
 
           <form
             onSubmit={handleSubmit}
-            className="flex bg-white p-2 rounded-md focus-within:ring-2
+            className="flex  mt-auto bg-white p-2 rounded-md focus-within:ring-2
             ring-offset-2 ring-secondary-600 "
           >
             {/* TODO - Handle resize */}
             <input
               placeholder="Write your message..."
-              className="placeholder-primary-300 resize-none
+              className="placeholder-primary-300 resize-none w-full
                 outline-none group-focus:ring-2 group-focus:ring-primary-400
               "
               value={message}
               onChange={(event) => setMessage(event.target.value)}
             />
 
-            <button type="button">
+            <button type="button" className="ml-auto">
               <AiOutlineFileImage
                 size={28}
                 className="text-primary-400 hover:text-primary-500"
               />
             </button>
 
-            <button
-              className="bg-secondary-100/80 rounded-full p-2"
-              type="submit"
-            >
+            <button className="bg-secondary-100 rounded-full p-2" type="submit">
               <AiOutlineSend
                 size={24}
                 className="text-primary-400 hover:text-primary-500"
