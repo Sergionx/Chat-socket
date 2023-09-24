9d467d95105aa2a9f0ee3b76dc249bc4c51590ab
Sergionx <sergio180203@gmail.com>
2023-09-24 17:29:11 -0400

    added counter in image and canSubmit

--- frontend/src/components/Input-Message.tsx
+++ frontend/src/components/Input-Message.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { AiOutlineFileImage, AiOutlineSend } from "react-icons/ai";
 import { Socket } from "socket.io-client";
 
 interface Props {
   socket: Socket;
   message: string;
+  imagesSelected: FileList | null;
   loadImages: (files: FileList | null) => void;
   handleFormSubmit: (
     event: React.FormEvent<HTMLFormElement>,
@@ -17,9 +18,14 @@ export default function InputMessage({
   socket,
   message,
   loadImages,
+  imagesSelected,
   handleFormSubmit,
   handleMessageChange,
 }: Props) {
+  const canSubmit: boolean = useMemo(() => {
+    return message.length > 0 || !!imagesSelected;
+  }, [message, imagesSelected]);
+
   return (
     <form
       onSubmit={(e) => handleFormSubmit(e, socket)}
@@ -30,8 +36,7 @@ export default function InputMessage({
       <input
         placeholder="Write your message..."
         className="placeholder-primary-300 resize-none w-full
-                outline-none group-focus:ring-2 group-focus:ring-primary-400
-              "
+          outline-none group-focus:ring-2 group-focus:ring-primary-400"
         value={message}
         onChange={handleMessageChange}
       />
@@ -43,14 +48,25 @@ export default function InputMessage({
         onChange={(event) => loadImages(event.target.files)}
       />
 
-      <label htmlFor="file" className="ml-auto cursor-pointer">
+      <label htmlFor="file" className="ml-auto cursor-pointer relative">
         <AiOutlineFileImage
           size={28}
           className="text-primary-400 hover:text-primary-500"
         />
+
+        {imagesSelected && imagesSelected.length >= 1 && (
+          <span className="absolute -top-2 -left-1 text-xs bg-primary-400 text-white rounded-full w-4 h-4 flex items-center justify-center">
+            {imagesSelected?.length}
+          </span>
+        )}
       </label>
 
-      <button className="ml-2 bg-secondary-100 rounded-full p-2" type="submit">
+      <button
+        disabled={!canSubmit}
+        className="ml-2 bg-secondary-100 rounded-full p-2 
+          disabled:opacity-50 disabled:cursor-not-allowed"
+        type="submit"
+      >
         <AiOutlineSend
           size={24}
           className="text-primary-400 hover:text-primary-500"
