bac0a4e3b61eade61ad04b8cbff49846f7ad3be3
Sergionx <sergio180203@gmail.com>
2023-09-25 19:27:05 -0400

    Tooltip to copy room code

--- frontend/src/pages/Chat-Room.tsx
+++ frontend/src/pages/Chat-Room.tsx
@@ -1,77 +1,76 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
-
-import Blobs from "../components/Blobs";
-
 import Messages from "../components/Messages";
 import SelectedImage from "../components/Selected-Image";
 
 import useMessages from "../hooks/useMessages";
 import useSelectedImage from "../hooks/useSelectedImage";
 import InputMessage from "../components/Input-Message";
-
-const socket = io("/", {
-  query: {
-    roomCode: "123",
-    roomPassword: "123",
-  },
-});
+import { Tooltip } from "react-tooltip";
+import { useState } from "react";
 
 export default function ChatRoom() {
-  
+  const [copyClicked, setCopyClicked] = useState(false);
+
   const {
     message,
     messages,
-    receiveMessage,
     imagesSelected,
     loadImages,
     handleMessageChange,
     handleFormSubmit,
+    socket,
+    roomCode,
   } = useMessages();
 
   const { selectedImage, handleCloseClick, handleImageClick } =
     useSelectedImage();
 
-  useEffect(() => {
-    socket.on("message", (message) => receiveMessage(message, true));
-
-    return () => turnOffSoccket();
-  }, []);
-
-  function turnOffSoccket() {
-    socket.off("message");
-  }
-
   return (
     <>
-      <div
-        className="bg-primary-400/40 min-h-screen  
-          grid place-items-center"
-      >
-        <Blobs />
-        <main
-          className="flex flex-col backdrop-blur-md bg-secondary-200/50 p-8 rounded-md
+      <main
+        className="flex flex-col backdrop-blur-md bg-secondary-200/50 p-8 rounded-md
           min-h-[80vh] w-[80vw] max-w-[600px] max-h-[600px] overflow-y-auto "
-        >
-          <h1 className="text-4xl font-bold mb-6">Private Chat</h1>
-
-          <Messages messages={messages} handleImageClick={handleImageClick} />
-
-          <InputMessage
-            socket={socket}
-            message={message}
-            handleFormSubmit={handleFormSubmit}
-            loadImages={loadImages}
-            handleMessageChange={handleMessageChange}
-            imagesSelected={imagesSelected}
-          />
-        </main>
-
-        <SelectedImage
-          selectedImage={selectedImage}
-          handleCloseClick={handleCloseClick}
+      >
+        <Tooltip
+          id="copy"
+          place="bottom"
+          content={copyClicked ? "Copied!" : "Click to copy"}
+          className="z-50"
+          delayHide={copyClicked ? 300 : 100}
+          clickable={true}
         />
-      </div>
+
+        <h1 className="text-4xl font-bold mb-6">
+          Private Chat
+          <span
+            data-tooltip-id="copy"
+            className="text-primary-500  font-normal ml-2 cursor-pointer"
+            onClick={() => {
+              navigator.clipboard.writeText(roomCode!);
+              setCopyClicked(true);
+              setTimeout(() => setCopyClicked(false), 1000);
+            }}
+          >
+            {" "}
+            ({roomCode})
+          </span>
+        </h1>
+
+        <Messages messages={messages} handleImageClick={handleImageClick} />
+
+        <InputMessage
+          socket={socket}
+          message={message}
+          handleFormSubmit={handleFormSubmit}
+          loadImages={loadImages}
+          handleMessageChange={handleMessageChange}
+          imagesSelected={imagesSelected}
+        />
+      </main>
+
+      <SelectedImage
+        selectedImage={selectedImage}
+        handleCloseClick={handleCloseClick}
+      />
     </>
   );
 }
