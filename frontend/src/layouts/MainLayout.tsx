5ec5c1c13f6400aaeb741eda19a7ac5ddbca37d1
Sergionx <sergio180203@gmail.com>
2023-09-25 18:57:15 -0400

    general layout for the app

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Blobs from "../components/Blobs";
+
+export default function MainLayout() {
+  return (
+    <div
+      className="bg-primary-400/40 min-h-screen
+        grid place-items-center"
+    >
+      <Blobs />
+      <Outlet />
+    </div>
+  );
+}
