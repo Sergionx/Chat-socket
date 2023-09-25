12d0809079cd2f25b32868a5068d6c73f85e99f0
Sergionx <sergio180203@gmail.com>
2023-09-25 19:27:20 -0400

    Better route handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
 import ChatRoom from "./pages/Chat-Room";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/room/:roomCode" element={<ChatRoom />} />
+      <Route path="*" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path=":roomCode" element={<ChatRoom />} />
+      </Route>
+      
       <Route path="*" element={<Error />} />
     </Routes>
   );
