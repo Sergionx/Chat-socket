65d574ce6897717b6c34130a5c24e773cdd188bc
Sergionx <sergio180203@gmail.com>
2023-10-02 11:12:07 -0400

    refactored to use redis

---
+++ backend/src/redis.ts
@@ -0,0 +1,8 @@
+import { createClient } from "redis";
+
+const redisClient = createClient();
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+redisClient.connect();
+export default redisClient;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,36 @@
 import express, { Request, Response } from "express";
 import { nanoid } from "nanoid";
+import redisClient from "./redis";
+
 const router = express.Router();
+
+router.post("/chat-room", async (req: Request, res: Response) => {
+  const { userName } = req.body;
+  let roomCode = nanoid(8);
+
+  try {
+    while (await redisClient.get(roomCode)) {
+      roomCode = nanoid(8);
+    }
+
+    const chatRoom = {
+      users: [userName],
+    };
+
+    await redisClient.set(roomCode, JSON.stringify(chatRoom), {
+      EX: 60 * 60 * 24,
+    });
+    console.log(roomCode);
+
+    res.json({ roomCode });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const chatRooms = {};
 
-router.post("/chat-room", (req: Request, res: Response) => {
+router.post("/chat-room-2", (req: Request, res: Response) => {
   const { userName } = req.body;
 
   let roomCode = nanoid(8);
