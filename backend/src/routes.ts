3cb7314dbca2ed948e39e9e9c37e674e3e46511c
Sergionx <sergio180203@gmail.com>
2023-10-12 22:00:26 -0400

    removed unused route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -38,23 +38,4 @@ router.post("/chat-room", async (req: Request, res: Response) => {
   }
 });
 
-const chatRooms = {};
-
-router.post("/chat-room-2", (req: Request, res: Response) => {
-  const { userName } = req.body;
-
-  let roomCode = nanoid(8);
-
-  while (chatRooms[roomCode]) {
-    roomCode = nanoid(8);
-  }
-
-  chatRooms[roomCode] = {
-    users: [userName],
-  };
-
-  res.json({ roomCode });
-  console.log(chatRooms);
-});
-
 export default router;
