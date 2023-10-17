8b2e06f13750cad981beb63872eb0152cfcea96e
Sergionx <sergio180203@gmail.com>
2023-10-16 21:30:30 -0400

    fixed port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ const io = new SocketServer(server);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
